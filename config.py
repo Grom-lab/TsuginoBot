ecb279efcab06609429c7339d6b20e292ce8ec36
Albatas <54707947+Grom-lab@users.noreply.github.com>
2025-04-15 20:32:03 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    RESPONSE_DELAY = (3, 6)  # Увеличены задержки
-    MAX_MESSAGE_LENGTH = 300
-    TEMPERATURE = 0.7  # Снижена случайность
-    TOP_P = 0.85
-    SAFETY_SETTINGS = {'HARASSMENT': 'BLOCK_NONE'}
-    REQUEST_INTERVAL = 5  # 5 секунд между запросами
